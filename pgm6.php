5e0a8cb8bea0c3f747a8ad77121b31a2afc995b1
Asna16 <99707864+Asna16@users.noreply.github.com>
2022-03-05 10:44:24 +0530

    Add files via upload

---
+++ pgm6.php
@@ -0,0 +1,14 @@
+    <?php  
+    setcookie("user", "asna");  
+    ?>  
+    <html>  
+    <body>  
+    <?php  
+    if(!isset($_COOKIE["user"])) {  
+        echo "Sorry, cookie is not found!";  
+    } else {  
+        echo "<br/>Cookie Value: " . $_COOKIE["user"];  
+    }  
+    ?>  
+    </body>  
+    </html>  
